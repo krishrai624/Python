2cd026fb66b9bb41acb8dcf6e4151c90d3234bb3
Krish Rai <46421812+krishrai624@users.noreply.github.com>
2022-07-01 17:00:08 +0545

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,9 +1,16 @@
+print("WELCOME TO THE WORDS WORLD.")
+print("RIGHT NOW! WE ONLY HAVE 3 WORDS, WE WILL ADD MORE IN FUTURE.\nYOU CAN ONLY USE GIVEN WORDS, THEY ARE: Food, Game, Fan. \nNOTE: WRITE THE GIVEN WORDS AS SHOWN.\n")
+dictionary = {"Food":"https://en.wikipedia.org/wiki/Food","Game":"https://en.wikipedia.org/wiki/Game","Fan":"https://en.wikipedia.org/wiki/Fan"}
+find = input("Enter the word given above:")
+print("Meaning of "+ find +":"+dictionary.get(find))
+
+----------------------------------------------------------------------------------------------------------
+Output:-
+  
 WELCOME TO THE WORDS WORLD.
 RIGHT NOW! WE ONLY HAVE 3 WORDS, WE WILL ADD MORE IN FUTURE.
 YOU CAN ONLY USE GIVEN WORDS, THEY ARE: Food, Game, Fan.
 NOTE: WRITE THE GIVEN WORDS AS SHOWN.
 
-----------------------------------------------------------------------------------------------------------
-Output:
 Enter the word given above:Food
 Meaning of Food:https://en.wikipedia.org/wiki/Food
